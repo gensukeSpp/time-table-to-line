c2baf523f44cce529734de69b0d896b01ef6118a
gensukeSpp <kali.n.utakata@gmail.com>
2026-08-14 15:51:58 +0900

    #260814: リリース 03

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()],
+  plugins: [vanillaExtractPlugin(), react(), cloudflare()],
   resolve: {},
   build: {
     chunkSizeWarningLimit: 100000000,
